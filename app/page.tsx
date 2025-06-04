1e62daf5d5a08b09b15dbb7d72f0584d9d0c943f
Enzo <enzo.voisin@devinfluence.fr>
2025-06-04 11:10:47 +0200

    Update welcome message in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">OK 4</a>
+          Welcome to <a href="https://nextjs.org">Antoine le gros bg qui nous prepare un truc mais on sait pas quoi soon !</a>
         </h1>
 
         <p className={styles.description}>
